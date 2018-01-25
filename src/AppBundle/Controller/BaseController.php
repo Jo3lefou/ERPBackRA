2fa0204faa8f53bbd00b72cb9ab38f39421247c0
Joffrey Faroux <joffrey.faroux@gmail.com>
2018-01-25 16:16:29 +0100

    Stat - Bloc Add more Stat

--- src/AppBundle/Controller/BaseController.php
+++ src/AppBundle/Controller/BaseController.php
@@ -23,7 +23,12 @@ use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
 class BaseController extends Controller
 {
 	public function getStatDataAction(){
+		// Serializer for date 
+		$serializer = new Serializer(array(new DateTimeNormalizer()));
+
 		// Total Revenue
+		$dateYear = $serializer->normalize( new \DateTime(), null, array(DateTimeNormalizer::FORMAT_KEY => 'y'));
+
 		$repositoryRevenue = $this->getDoctrine()->getRepository(RarModelOrdered::class);
 		$querySumPerShop = $repositoryRevenue->createQueryBuilder('r')
             ->select("SUM(r.prixSoldHT) as amount")
@@ -31,26 +36,44 @@ class BaseController extends Controller
             ->leftJoin('o.shop', 's')
             ->where('o.state NOT IN (:status)')
             ->andWhere('s.isActive = 1')
+            ->andWhere('o.year = '.$dateYear)
             ->setParameters(['status' => array(0, 1, 4)]);
         $sumRevenue = $querySumPerShop->getQuery()->getResult();
 
         $em = $this->getDoctrine()->getManager();
 	    $qb = $em->createQueryBuilder();
+
+	    // Nb Total Order
+	    $nbOrderTotal = $qb->select('COUNT(p)')
+	                ->from('AppBundle:RarOrder' , 'p')
+	                ->where('p.state NOT IN (:pstate)')
+	                ->setParameters(['pstate' => array(0, 1, 4)])
+	                ->getQuery()
+	                ->getOneOrNullResult();
+
 	    // Nb Order Pending
 	    $nbOrderPending = $qb->select('COUNT(r)')
 	                ->from('AppBundle:RarOrder' , 'r')
-	                ->where('r.state = 1')
+	                ->where('r.state = :rstate')
+	                ->setParameters(['rstate' => '1'])
 	                ->getQuery()
 	                ->getOneOrNullResult();
+
+	    // Nb Customer
+	    $nbCustomerTotal = $qb->select('COUNT(c)')
+	                ->from('AppBundle:RarCustomer' , 'c')
+	                ->getQuery()
+	                ->getOneOrNullResult();
+
+
 		// Nb Order to deliver this month
-	    $serializer = new Serializer(array(new DateTimeNormalizer()));
 	    $dateAsString = $serializer->normalize( new \DateTime('+ 1 month'), null, array(DateTimeNormalizer::FORMAT_KEY => 'd/m/Y'));
 
 	    $nbOrderToDeliverThisMonth = $qb->select('COUNT(o)')
 	                ->from('AppBundle:RarOrder' , 'o')
-	                ->where('o.state NOT IN (:status)')
-	                ->andWhere('o.dateMaxShip < :date')
-	                ->setParameters(['status' => array(0, 1, 4, 6), 'date' => $dateAsString])
+	                ->where('o.state NOT IN (:ostatus)')
+	                ->andWhere('o.dateMaxShip < :odate')
+	                ->setParameters(['ostatus' => array(0, 1, 4, 6), 'odate' => $dateAsString])
 	                ->getQuery()
 	                ->getOneOrNullResult();
 
@@ -58,6 +81,8 @@ class BaseController extends Controller
 		$stat['sumRevenue'] = $sumRevenue;
 		$stat['nbOrderPending'] = $nbOrderPending;
 		$stat['nbOrderToDeliverThisMonth'] = $nbOrderToDeliverThisMonth;
+		$stat['nbOrderTotal'] = $nbOrderTotal;
+		$stat['nbCustomerTotal'] = $nbCustomerTotal;
 
 		// bloc rendered
 		return $this->render('bloc/bloc-stat.html.twig', array('stat' => $stat));
