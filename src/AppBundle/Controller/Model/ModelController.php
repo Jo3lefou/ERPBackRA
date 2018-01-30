397da8c34ca107934a7c4e91b7413bdf73cea418
Joffrey Faroux <joffrey.faroux@gmail.com>
2018-01-30 23:40:08 +0100

    Model - Search efficient

--- src/AppBundle/Controller/Model/ModelController.php
+++ src/AppBundle/Controller/Model/ModelController.php
@@ -46,7 +46,7 @@ class ModelController extends Controller
 
                 $term = $request->query->get('term');
                 $em = $this->get('doctrine.orm.entity_manager');
-                $dql = "SELECT a FROM AppBundle:RarModel a WHERE a.name LIKE '%".addcslashes($term, '%_')."%' ORDER BY a.name";
+                $dql = "SELECT a FROM AppBundle:RarModel a WHERE a.name LIKE '%".addcslashes($term, '%_')."%' OR a.sku LIKE '%".addcslashes($term, '%_')."%' ORDER BY a.name";
                 $query = $em->createQuery($dql);
                 $paginator  = $this->get('knp_paginator');
                 $pagination = $paginator->paginate( $query, $request->query->getInt('page', 1), $number );
