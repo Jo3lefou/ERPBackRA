190cbc45ea0ad00e8a726fa3c6ae1be6bae2a222
Joffrey Faroux <joffrey.faroux@gmail.com>
2018-01-31 19:20:05 +0100

    Debug Test

--- src/AppBundle/Controller/Model/ModelController.php
+++ src/AppBundle/Controller/Model/ModelController.php
@@ -40,16 +40,14 @@ class ModelController extends Controller
             $pagination = $paginator->paginate( $query, $request->query->getInt('page', 1), $number );
         }elseif($display == 'search'){
             $request = new Request($_POST);
-            if($request != ''){
-                if ($request->isMethod('POST')) {
-                    $term = $request->query->get('term');
-                    $em = $this->get('doctrine.orm.entity_manager');
-                    $dql = "SELECT a FROM AppBundle:RarModel a WHERE a.name LIKE '%".addcslashes($term, '%_')."%'";
-                    $query = $em->createQuery($dql);
-                    $paginator  = $this->get('knp_paginator');
-                    $pagination = $paginator->paginate( $query, $request->query->getInt('page', 1), $number );
-                    $searchTerm = $term;
-                }
+            if ($request->isMethod('POST')) {
+                $term = $request->query->get('term');
+                $em = $this->get('doctrine.orm.entity_manager');
+                $dql = "SELECT a FROM AppBundle:RarModel a WHERE a.name LIKE '%".addcslashes($term, '%_')."%'";
+                $query = $em->createQuery($dql);
+                $paginator  = $this->get('knp_paginator');
+                $pagination = $paginator->paginate( $query, $request->query->getInt('page', 1), $number );
+                $searchTerm = $term;
             }else{
                 return $this->redirect('/admin/'.$locale.'/models/view/'.$number);
             }
