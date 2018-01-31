aa47a59668ed004df09b37ed663d23003105edfc
Joffrey Faroux <joffrey.faroux@gmail.com>
2018-01-31 18:44:16 +0100

    Search Model - Controler Rectification

--- src/AppBundle/Controller/Model/ModelController.php
+++ src/AppBundle/Controller/Model/ModelController.php
@@ -30,6 +30,8 @@ class ModelController extends Controller
         $searchTerm = "";
         $pagination = array();
 
+
+
         if($display == 'view'){
             $em = $this->get('doctrine.orm.entity_manager');
             $dql = "SELECT a FROM AppBundle:RarModel a ORDER BY a.name";
@@ -37,21 +39,20 @@ class ModelController extends Controller
             $paginator  = $this->get('knp_paginator');
             $pagination = $paginator->paginate( $query, $request->query->getInt('page', 1), $number );
         }elseif($display == 'search'){
-
-            //$request = Request::createFromGlobals();
-
-            if ($request->isMethod('POST')) {
-
-                $request = new Request($_POST);
-
-                $term = $request->query->get('term');
-                $em = $this->get('doctrine.orm.entity_manager');
-                $dql = "SELECT a FROM AppBundle:RarModel a WHERE a.name LIKE '%".addcslashes($term, '%_')."%' OR a.sku LIKE '%".addcslashes($term, '%_')."%' ORDER BY a.name";
-                $query = $em->createQuery($dql);
-                $paginator  = $this->get('knp_paginator');
-                $pagination = $paginator->paginate( $query, $request->query->getInt('page', 1), $number );
-                $searchTerm = $term;
+            $request = new Request($_POST);
+            if($request != ''){
+                if ($request->isMethod('POST')) {
+                    $term = $request->query->get('term');
+                    $em = $this->get('doctrine.orm.entity_manager');
+                    $dql = "SELECT a FROM AppBundle:RarModel a WHERE a.name LIKE '%".addcslashes($term, '%_')."%' OR a.sku LIKE '%".addcslashes($term, '%_')."%' ORDER BY a.name";
+                    $query = $em->createQuery($dql);
+                    $paginator  = $this->get('knp_paginator');
+                    $pagination = $paginator->paginate( $query, $request->query->getInt('page', 1), $number );
+                    $searchTerm = $term;
+                }
             }
+        }else{
+            return $this->redirect('/admin/'.$locale.'/models/view/'.$number);
         }
 
 
