fa365f356ff873f17dbe0b1bf94a6b6107e7e56a
Joffrey Faroux <joffrey.faroux@gmail.com>
2018-02-28 19:48:05 +0100

    Meetings - DataTable and Templating finished.

--- src/AppBundle/Controller/User/ProfileController.php
+++ src/AppBundle/Controller/User/ProfileController.php
@@ -18,15 +18,23 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 class ProfileController extends Controller
 {
     /**
-     * @Route("/{_locale}/profile/", name="profile")
+     * @Route("/{_locale}/profile/{id}", defaults={"id"="null"}, name="profile")
      */
-    public function indexAction()
+    public function indexAction($id)
     {
         $user = $this->get('security.token_storage')->getToken()->getUser();
         $firstName = $this->getUser()->getLastName();
         $lastName = $this->getUser()->getFirstName();
         $locale = $this->getUser()->getLocale();
         $name = $firstName.' '.$lastName;
+
+
+        if($id == "null"){
+            $usertoDisplay = $user;
+        }else{
+            $em = $this->getDoctrine()->getManager();
+            $usertoDisplay = $em->getRepository('AppBundle:User')->find($id);
+        }
 
         if($user){
             return $this->render('users/profile/profile.html.twig', array(
@@ -36,6 +44,7 @@ class ProfileController extends Controller
                 'h1title' => $this->get('translator')->trans('Welcome User'),
                 'bodyClass' => 'nav-md',
                 'user' => $user,
+                'usertodisplay' => $usertoDisplay
             ));
         }else{
             return $this->redirect('login');
